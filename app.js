d371693be57babe8a566cddcad7f305702af3393
Jackson <jpwoods92@gmail.com>
2018-11-05 15:54:09 -0500

    .gitignore fix

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
-const express = require('express')
-const chalk = require('chalk')
-const app = express()
-const debug = require('debug')('app')
-const morgan = require('morgan')
-const path = require('path')
+const express = require('express');
+const chalk = require('chalk');
 
-app.use(morgan('tiny'))
-app.use(express.static(path.join(__dirname, '/public')))
-app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')))
-app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')))
-app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')))
+const app = express();
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
 
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, '/views/index.html'))
-})
+app.use(morgan('tiny'));
+app.use(express.static(path.join(__dirname, '/public')));
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
 
-app.listen(3000, function () {
-  debug(`listening on port ${chalk.green('3000')}`)
-})
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/index.html'));
+});
+
+app.listen(3000, () => {
+  debug(`listening on port ${chalk.green('3000')}`);
+});
