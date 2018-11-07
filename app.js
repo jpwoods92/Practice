a2bb9a6cefc229d548c649abefb6e08d00f7c5b4
Jackson <jpwoods92@gmail.com>
2018-11-07 17:08:40 -0500

    add routing

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const chalk = require('chalk');
 
 const app = express();
 const port = process.env.PORT || 3000;
+const bookRouter = express.Router();
+
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
@@ -14,8 +16,39 @@ app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
+
+const books = [
+  {
+    title: 'War and Peace',
+    genre: 'Historical Fiction',
+    author: 'Lev Nikolayevich Tolstoy',
+    read: false,
+  },
+];
+bookRouter.route('/')
+  .get((req, res) => {
+    res.render('books',
+      {
+        title: 'Library',
+        nav: [{ link: '/books', title: 'Books' },
+          { link: '/authors', title: 'Authors' }],
+        books,
+      });
+  });
+
+bookRouter.route('/single')
+  .get((req, res) => {
+    res.send('hello single book');
+  });
+
+app.use('/books', bookRouter);
 app.get('/', (req, res) => {
-  res.render('index', { title: 'Library', list: ['a', 'b'] });
+  res.render('index',
+    {
+      title: 'Library',
+      nav: [{ link: '/books', title: 'Books' },
+        { link: '/authors', title: 'Authors' }],
+    });
 });
 
 app.listen(port, () => {
