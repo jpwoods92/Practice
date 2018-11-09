6108bb84f3d6bee0144408c3feb9d6c9894e9152
Jackson <jpwoods92@gmail.com>
2018-11-09 15:44:15 -0500

    update nav and create router function

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -2,37 +2,39 @@ const express = require('express');
 
 const bookRouter = express.Router();
 
-const books = [
-  {
-    title: 'War and Peace',
-    genre: 'Historical Fiction',
-    author: 'Lev Nikolayevich Tolstoy',
-    read: false,
-  },
-];
-bookRouter.route('/')
-  .get((req, res) => {
-    res.render('bookListView',
-      {
-        title: 'Library',
-        nav: [{ link: '/books', title: 'Books' },
-          { link: '/authors', title: 'Authors' }],
-        books,
-      });
-  });
+function router(nav) {
+  const books = [
+    {
+      title: 'War and Peace',
+      genre: 'Historical Fiction',
+      author: 'Lev Nikolayevich Tolstoy',
+      read: false,
+    },
+  ];
+  bookRouter.route('/')
+    .get((req, res) => {
+      res.render('bookListView',
+        {
+          title: 'Library',
+          nav,
+          books,
+        });
+    });
 
-bookRouter.route('/:id')
-  .get((req, res) => {
-    const { id } = req.params;
-    res.render(
-      'bookView',
-      {
-        title: 'Library',
-        nav: [{ link: '/books', title: 'Books' },
-          { link: '/authors', title: 'Authors' }],
-        book: books[id],
-      },
-    );
-  });
+  bookRouter.route('/:id')
+    .get((req, res) => {
+      const { id } = req.params;
+      res.render(
+        'bookView',
+        {
+          title: 'Library',
+          nav,
+          book: books[id],
+        },
+      );
+    });
+  return bookRouter;
+}
 
-module.exports = bookRouter;
+
+module.exports = router;
